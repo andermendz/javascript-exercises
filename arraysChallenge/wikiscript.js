13d4781096713188480a03431c980829a6beeb2d
andermendz <andersonmendz19@gmail.com>
2023-01-18 19:05:04 -0500

    search method for array

--- arraysChallenge/wikiscript.js
+++ arraysChallenge/wikiscript.js
@@ -1,1 +1,15 @@
 console.log("jeje")
+const doc = Array.from(document.querySelectorAll(" .mw-category-group li"))
+
+
+console.log(doc)
+
+doc.forEach((d)=>{
+    console.log(d.textContent)
+    }
+)
+
+let v = "dadadadadqs"
+
+let s = v.search("qs")
+console.log(s)
